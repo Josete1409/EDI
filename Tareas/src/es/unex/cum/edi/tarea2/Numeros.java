817576ae8b1d82e7582d83051db70dcdfd2b4eba
Jose Antonio Alvarez Nieto <josete1409@gmail.com>
2018-02-11 03:38:05 +0100

    Numeros

--- Tareas/src/es/unex/cum/edi/tarea2/Numeros.java
+++ Tareas/src/es/unex/cum/edi/tarea2/Numeros.java
@@ -1,10 +1,36 @@
 package es.unex.cum.edi.tarea2;
 
+import java.util.Scanner;
+
 public class Numeros {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String cad = " ", num = "0123456789";
+		int cont = 0;
+		
+		Scanner in = new Scanner(System.in);
+		
+		cad = in.nextLine();
+		in.close();
+		
+		for(int i=0; i<cad.length(); i++) {
+			
+			for(int j=0; j<num.length(); j++) {
+				
+				if(cad.charAt(i) == num.charAt(j)) {
+					
+					cont++;
+					
+				}
+				
+			}
+			
+		}
+		
+		System.out.println("La cadena tiene "+cont+" dígitos");
+		
 	}
 
 }
